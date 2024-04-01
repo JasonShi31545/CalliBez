071d5977a165d175e78e67eea647a33c43ca6ae8
JasonShi31545Personal <shibingzhao@outlook.com>
2024-04-01 18:54:32 +0800

    Cool pattern

--- main.cc
+++ main.cc
@@ -1,36 +1,75 @@
 #include "setup.h"
 
+Point pp, pq, pr, ps, pu, pv, py, pz;
+Point p, q, r, s, u, v, y, z;
+Point _p0, _p1, _p2;
+float w;
 
-void Calculate(PixelGrid *grid, float t) {
+const float SHIFT_X = W_WIDTH/2.0f;
+const float SHIFT_Y = W_HEIGHT/2.0f;
 
-    Point _p0 = Point{100, 100};
-    float alpha = 1.05f;
-    std::tuple<HomogeneousPoint, Point, float> circ = ConstructArc(_p0, alpha);
-    HomogeneousPoint p1 = std::get<0>(circ);
-    Point _p1 = Point{p1.x, p1.y};
-    float w = p1.w;
+const float SCALE_X = 2.0f;
+const float SCALE_Y = 1.0f;
 
-    Point _p2 = std::get<1>(circ);
+const float SHEAR_X = 2.0f;
 
-    std::vector<Point> points = {_p0, _p1, _p2};
-    std::vector<float> weights1 = {1.0f, w, 1.0f};
-    std::vector<float> weights2 = {1.0f, -w, 1.0f};
+const float ROTATE_ANGLE = M_PI_4f;
 
-    Point p = BezierCurveRationalWeighted(2, points, weights1, t);
-    p = LinearScale(p, 2.0f, 1.0f);
-    p = ShiftCoordinate(p, 300, 300);
+void Update(PixelGrid *grid, float t) {
 
-    Point q = BezierCurveRationalWeighted(2, points, weights2, t);
-    q = LinearScale(q, 2.0f, 1.0f);
-    q = ShiftCoordinate(q, 300, 300);
+    p = ShiftCoordinate(LinearRotate(LinearScale(BSRQS(_p0, _p1, _p2, w, t), SCALE_X, SCALE_Y), ROTATE_ANGLE), SHIFT_X, SHIFT_Y);
+    q = ShiftCoordinate(LinearRotate(LinearScale(BSRQS(_p0, _p1, _p2, -w, t), SCALE_X, SCALE_Y), ROTATE_ANGLE), SHIFT_X, SHIFT_Y);
 
-    DrawPoint(*grid, p);
-    DrawPoint(*grid, q);
+    r = ShiftCoordinate(LinearScale(BSRQS(_p0, _p1, _p2, w, t), SCALE_X, SCALE_Y), SHIFT_X, SHIFT_Y);
+    s = ShiftCoordinate(LinearScale(BSRQS(_p0, _p1, _p2, -w, t), SCALE_X, SCALE_Y), SHIFT_X, SHIFT_Y);
+
+    u = ShiftCoordinate(LinearRotate(LinearScale(BSRQS(_p0, _p1, _p2, w, t), SCALE_X, SCALE_Y), -ROTATE_ANGLE), SHIFT_X, SHIFT_Y);
+    v = ShiftCoordinate(LinearRotate(LinearScale(BSRQS(_p0, _p1, _p2, -w, t), SCALE_X, SCALE_Y), -ROTATE_ANGLE), SHIFT_X, SHIFT_Y);
+
+    y = ShiftCoordinate(LinearRotate(LinearScale(BSRQS(_p0, _p1, _p2, w, t), SCALE_X, SCALE_Y), M_PI_2f), SHIFT_X, SHIFT_Y);
+    z = ShiftCoordinate(LinearRotate(LinearScale(BSRQS(_p0, _p1, _p2, -w, t), SCALE_X, SCALE_Y), M_PI_2f), SHIFT_X, SHIFT_Y);
+
+    DrawLine(*grid, p, pp);
+    DrawLine(*grid, q, pq);
+    DrawLine(*grid, r, pr);
+    DrawLine(*grid, s, ps);
+    DrawLine(*grid, u, pu);
+    DrawLine(*grid, v, pv);
+    DrawLine(*grid, y, py);
+    DrawLine(*grid, z, pz);
+
+    pp = p;
+    pq = q;
+    pr = r;
+    ps = s;
+    pu = u;
+    pv = v;
+    py = y;
+    pz = z;
 }
 
 
 
 int main(int argc, const char *argv[]) {
-    SetupAndLoop(Calculate);
+
+    _p0 = Point{100, 100};
+    float alpha = 1.05f;
+    std::tuple<HomogeneousPoint, Point, float> circ = ConstructArc(_p0, alpha);
+    HomogeneousPoint p1 = std::get<0>(circ);
+    _p1 = Point{p1.x, p1.y};
+    w = p1.w;
+
+    _p2 = std::get<1>(circ);
+
+    pp = ShiftCoordinate(LinearRotate(LinearScale(_p0, SCALE_X, SCALE_Y), ROTATE_ANGLE), SHIFT_X, SHIFT_Y);
+    pq = pp;
+    pr = ShiftCoordinate(LinearScale(_p0, SCALE_X, SCALE_Y), SHIFT_X, SHIFT_Y);
+    ps = pr;
+    pu = ShiftCoordinate(LinearRotate(LinearScale(_p0, SCALE_X, SCALE_Y), -ROTATE_ANGLE), SHIFT_X, SHIFT_Y);
+    pv = pu;
+    py = ShiftCoordinate(LinearRotate(LinearScale(_p0, SCALE_X, SCALE_Y), M_PI_2f), SHIFT_X, SHIFT_Y);
+    pz = py;
+
+    SetupAndLoop(Update);
     return 0;
 }
